80c251db20938088d384d4b1702a804fefac6255
ropimasi <ropimasi@email.com>
2026-02-04 14:27:31 -0300

    @PostMapping to creatTask is done.

--- src/main/java/dev/ropimasi/taskmanager/api/controller/TaskController.java
+++ src/main/java/dev/ropimasi/taskmanager/api/controller/TaskController.java
@@ -2,8 +2,11 @@ package dev.ropimasi.taskmanager.api.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import dev.ropimasi.taskmanager.api.entity.Task;
@@ -23,6 +26,13 @@ public class TaskController {
 	public ResponseEntity<List<Task>> listAll() {
 		List<Task> tasks = taskRepository.findAll();
 		return ResponseEntity.ok(tasks);
+	}
+
+
+	@PostMapping
+	public ResponseEntity<Task> createTask(@RequestBody Task task) {
+		Task savedTask = taskRepository.save(task);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
 	}
 
 }
