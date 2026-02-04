ea46071234db1fc93dceca05a7db9123d660cb4c
ropimasi <ropimasi@email.com>
2026-02-04 15:04:45 -0300

    TaskCreatRequestDTO adapted for createTask.

--- src/main/java/dev/ropimasi/taskmanager/api/controller/TaskController.java
+++ src/main/java/dev/ropimasi/taskmanager/api/controller/TaskController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import dev.ropimasi.taskmanager.api.dto.TaskCreateRequestDTO;
 import dev.ropimasi.taskmanager.api.entity.Task;
 import dev.ropimasi.taskmanager.api.repository.TaskRepository;
+import jakarta.validation.Valid;
 
 
 
@@ -30,8 +32,13 @@ public class TaskController {
 
 
 	@PostMapping
-	public ResponseEntity<Task> createTask(@RequestBody Task task) {
-		Task savedTask = taskRepository.save(task);
+	public ResponseEntity<Task> createTask(@RequestBody @Valid TaskCreateRequestDTO taskDTO) {
+
+		Task taskToSave = new Task();
+		taskToSave.setTitle(taskDTO.title());
+		taskToSave.setDescription(taskDTO.description());
+
+		Task savedTask = taskRepository.save(taskToSave);
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
 	}
 
---
+++ src/main/java/dev/ropimasi/taskmanager/api/dto/TaskCreateRequestDTO.java
@@ -0,0 +1,12 @@
+package dev.ropimasi.taskmanager.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record TaskCreateRequestDTO(
+		@NotBlank(message = "Title is required")
+		@Size(min = 1, max = 128, message = "Title must be between 1 and 128 characters")
+		String title,
+		String description
+		) {
+}
