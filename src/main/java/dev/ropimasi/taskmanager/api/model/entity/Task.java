d8bd90d2f4a46aa61f9ad3613bfe9f6ba9af31ce
ropimasi <ropimasi@email.com>
2026-02-04 15:20:01 -0300

    Lombok adopted for Task entity.

--- src/main/java/dev/ropimasi/taskmanager/api/model/entity/Task.java
+++ src/main/java/dev/ropimasi/taskmanager/api/model/entity/Task.java
@@ -8,9 +8,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 //@Table(name = "task")
 public class Task {
@@ -35,59 +41,10 @@ public class Task {
 	@UpdateTimestamp
 	private LocalDateTime updatedAt;
 
-	// --- Construtores ---
-	public Task() {
-	}
-
 
 	public Task(String title, String description) {
 		this.title = title;
 		this.description = description;
 	}
 
-
-	// --- Getters e Setters ---
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public String getTitle() {
-		return title;
-	}
-
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-
-	public String getDescription() {
-		return description;
-	}
-
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-
-	public boolean isCompleted() {
-		return completed;
-	}
-
-
-	public void setCompleted(boolean completed) {
-		this.completed = completed;
-	}
-
-
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
 }
